0b4cd429c6adaa736a0f71c32a9efa4b35d27455
food designer <maxwurueian@gmail.com>
2026-06-01 16:38:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('searchBtn').addEventListener('click', searchSports);
 
-// 🎯 真實資料庫：完美對齊 center (運動中心)、school (國中小校園)、court (羽網球場)
-const sportsDatabase = {
+// 🎯 真實資料庫：center (運動中心)、school (國中小校園)、court (羽網球場)
+var sportsDatabase = {
   "基隆市": [
     { name: "基隆市國民運動中心", address: "基隆市仁愛區仁愛路19號", hours: "06:00–22:00", type: "center", typeText: "綜合/國民運動中心" },
     { name: "信義國民小學體育館", address: "基隆市信義區仁二路135號", hours: "配合學校開放時間", type: "school", typeText: "籃球/體育館/校園" },
@@ -69,3 +69,109 @@ const sportsDatabase = {
     { name: "嘉義縣立體育館", address: "嘉義縣朴子市朴子七路1號", hours: "08:00–17:00", type: "center", typeText: "綜合/國民運動中心" },
     { name: "朴子國民中學", address: "嘉義縣朴子市大同路4號", hours: "配合學校開放時間", type: "school", typeText: "籃球/體育館/校園" },
     { name: "大林國民小學運動場", address: "嘉義縣大林鎮中興路2號", hours: "課後與假日開放", type: "school", typeText: "籃球/體育館/校園" }
+  ],
+  "台南市": [
+    { name: "永華國民運動中心", address: "台南市中西區中華西路二段30號", hours: "06:00–22:00", type: "center", typeText: "綜合/國民運動中心" },
+    { name: "大成國民中學風雨球場", address: "台南市南區西門路一段306號", hours: "配合學校開放時間", type: "school", typeText: "籃球/體育館/校園" },
+    { name: "勝利國民小學操場球場", address: "台南市東區勝利路10號", hours: "課後及假日開放", type: "school", typeText: "籃球/體育館/校園" },
+    { name: "後甲國民中學網球場", address: "台南市東區東平路260號", hours: "依校方公告為準", type: "court", typeText: "羽毛球/網球場" }
+  ],
+  "高雄市": [
+    { name: "鳳山運動園區國民運動中心", address: "高雄市鳳山區光華路68號", hours: "06:00–22:00", type: "center", typeText: "綜合/國民運動中心" },
+    { name: "苓雅運動中心 (技擊館)", address: "高雄市苓雅區中正一路96號", hours: "06:00–22:00", type: "center", typeText: "綜合/國民運動中心" },
+    { name: "五福國民中學體育場", address: "高雄市苓雅區五福一路12號", hours: "課後與假日開放", type: "school", typeText: "籃球/體育館/校園" },
+    { name: "信義國民小學風雨球場", address: "高雄市新興區中正三路172號", hours: "假日局部開放", type: "school", typeText: "籃球/體育館/校園" }
+  ],
+  "屏東縣": [
+    { name: "屏東國民運動中心", address: "屏東縣屏東市勝利東路50號", hours: "06:00–22:00", type: "center", typeText: "綜合/國民運動中心" },
+    { name: "屏東國民中學體育館", address: "屏東縣屏東市大連路70號", hours: "配合學校開放時間", type: "school", typeText: "籃球/體育館/校園" },
+    { name: "仁愛國民小學運動場", address: "屏東縣屏東市仁愛路98號", hours: "課後及假日開放", type: "school", typeText: "籃球/體育館/校園" }
+  ],
+  "宜蘭縣": [
+    { name: "宜蘭國民運動中心", address: "宜蘭縣宜蘭市公園路53號", hours: "06:00–22:00", type: "center", typeText: "綜合/國民運動中心" },
+    { name: "宜蘭市立運動公園籃球場", address: "宜蘭縣宜蘭市中山路五段50號", hours: "24小時營業", type: "court", typeText: "羽毛球/網球場" },
+    { name: "宜蘭國民中學體育館", address: "宜蘭縣宜蘭市樹人路37號", hours: "配合學校開放時間", type: "school", typeText: "籃球/體育館/校園" },
+    { name: "中山國民小學球場", address: "宜蘭縣宜蘭市崇聖街4號", hours: "假日開放", type: "school", typeText: "籃球/體育館/校園" }
+  ],
+  "花蓮縣": [
+    { name: "花蓮縣立體育館 (小巨蛋)", address: "花蓮縣花蓮市達固湖灣大路23號", hours: "08:00–22:00", type: "center", typeText: "綜合/國民運動中心" },
+    { name: "國風國民中學體育館", address: "花蓮縣花蓮市林政街169號", hours: "配合學校開放時間", type: "school", typeText: "籃球/體育館/校園" },
+    { name: "明義國民小學活動中心", address: "花蓮縣花蓮市明義街107號", hours: "依學校開放公告", type: "school", typeText: "籃球/體育館/校園" }
+  ],
+  "台東縣": [
+    { name: "台東縣立體育館", address: "台東縣台東市桂林北路201號", hours: "08:00–17:30", type: "center", typeText: "綜合/國民運動中心" },
+    { name: "新生國民中學體育館", address: "台東縣台東市新生路641號", hours: "配合學校開放時間", type: "school", typeText: "籃球/體育館/校園" },
+    { name: "新生國民小學操場", address: "台東縣台東市更生路474巷45號", hours: "課後與假日開放", type: "school", typeText: "籃球/體育館/校園" }
+  ],
+  "澎湖縣": [
+    { name: "澎湖縣綜合體育館", address: "澎湖縣馬公市文化路31號", hours: "08:00–21:30", type: "center", typeText: "綜合/國民運動中心" },
+    { name: "馬公國民中學體育館", address: "澎湖縣馬公市陽明路115號", hours: "配合學校開放時間", type: "school", typeText: "籃球/體育館/校園" },
+    { name: "馬公國民小學操場球場", address: "澎湖縣馬公市光明路62號", hours: "假日特定時間開放", type: "school", typeText: "籃球/體育館/校園" }
+  ],
+  "金門縣": [
+    { name: "金門縣立體育館", address: "金門縣金城鎮民族路261號", hours: "08:00–21:30", type: "center", typeText: "綜合/國民運動中心" },
+    { name: "金城國民中學體育館", address: "金門縣金城鎮民權路32號", hours: "配合學校開放時間", type: "school", typeText: "籃球/體育館/校園" },
+    { name: "金門高中體育館與球場", address: "金門縣金城鎮光前路94號", hours: "配合校方規範開放", type: "school", typeText: "籃球/體育館/校園" }
+  ],
+  "連江縣": [
+    { name: "連江縣立南竿體育館", address: "連江縣南竿鄉介壽村254-2號", hours: "08:00–21:00", type: "center", typeText: "綜合/國民運動中心" },
+    { name: "介壽國民中小學體育場", address: "連江縣南竿鄉介壽村262號", hours: "配合學校開放時間", type: "school", typeText: "籃球/體育館/校園" },
+    { name: "福澳運動場籃球場", address: "連江縣南竿鄉福澳村", hours: "開放空間全天營業", type: "school", typeText: "籃球/體育館/校園" }
+  ]
+};
+
+function searchSports() {
+  var selectedCity = document.getElementById('citySelect').value;
+  var selectedSport = document.getElementById('sportSelect').value;
+  
+  var loading = document.getElementById('loading');
+  var resultsDiv = document.getElementById('results');
+  
+  loading.classList.remove('hidden');
+  resultsDiv.innerHTML = '';
+  
+  setTimeout(function() {
+    loading.classList.add('hidden');
+    
+    var allVenues = sportsDatabase[selectedCity] || [];
+    
+    // 篩選邏輯
+    var filteredVenues = allVenues.filter(function(venue) {
+      if (selectedSport === "all") return true;
+      return venue.type === selectedSport;
+    });
+    
+    if (filteredVenues.length === 0) {
+      resultsDiv.innerHTML = '<p style="color:#e74c3c; text-align:center; font-weight:bold; padding: 20px;">❌ 該縣市在此分類下暫無資料，請更換分類試試看！</p>';
+      return;
+    }
+    
+    filteredVenues.forEach(function(venue) {
+      var searchQuery = '台灣 ' + selectedCity + ' ' + venue.name;
+      
+      // 🛠️ 【核心修正】：放棄使用會被吃掉的 $ 符號，改用標準 '+' 號字串拼接，網址 100% 正常渲染！
+      var embedMapUrl = 'https://maps.google.com/maps?q=' + encodeURIComponent(searchQuery) + '&t=&z=15&ie=UTF8&iwloc=&output=embed';
+      var addressGoogleMapUrl = 'https://www.google.com/maps/search/?api=1&query=' + encodeURIComponent(selectedCity + venue.address);
+      
+      // 建立卡片元件
+      var card = document.createElement('div');
+      card.className = 'card';
+      
+      // 組合 HTML 內容
+      var cardHTML = '';
+      cardHTML += '<h3>📍 ' + venue.name + ' <span class="tag tag-' + venue.type + '">' + venue.typeText + '</span></h3>';
+      cardHTML += '<p><strong>🏠 詳細地址：</strong>';
+      cardHTML += '  <a href="' + addressGoogleMapUrl + '" target="_blank" class="review-link" title="點擊直接開啟 Google Map 導航">';
+      cardHTML += '    ' + venue.address + ' 🔗 (點我開啟 Google 地圖真實導航)';
+      cardHTML += '  </a>';
+      cardHTML += '</p>';
+      cardHTML += '<p><strong>⏰ 開放時間：</strong> ' + venue.hours + '</p>';
+      cardHTML += '<div class="map-container">';
+      cardHTML += '  <iframe width="100%" height="100%" src="' + embedMapUrl + '" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>';
+      cardHTML += '</div>';
+      
+      card.innerHTML = cardHTML;
+      resultsDiv.appendChild(card);
+    });
+  }, 300);
+}
